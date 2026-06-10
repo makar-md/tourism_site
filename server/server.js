734a417117aa2c85361b923456c9a361b15341eb
makar-md <150604154+makar-md@users.noreply.github.com>
2026-06-10 19:14:37 +0300

    add base struct

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express, { json } from 'express';
+const PORT = 3000
+const app = express()
+app.use(json)
+
+async function main(){
+
+    app.get('/get', (req,res) =>{
+        res.status(200).json({
+            message: "hello"
+        })
+    })
+    app.listen(PORT, ()=>{
+        console.log(`server start on ${PORT} port`)
+    })
+}
+
+main()
