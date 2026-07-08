12d4d16f1551067b0eae5cd7a1195da44c3e8877
makar-md <150604154+makar-md@users.noreply.github.com>
2026-07-08 17:23:25 +0300

    add readme

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ dotenv.config()
 //========== Express app ==========//
 const app = express()
 app.use(cors({
-        origin: 'http://localhost:5173',
+        origin: 'http://localhost:4173',
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     })
